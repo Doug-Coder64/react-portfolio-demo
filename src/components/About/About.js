3016e0e01b3ea714d08728df45874a5fb8804b82
Doug Peterson <douglaspeterson6@gmail.com>
2021-12-18 17:41:50 -0600

    finished footer

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 function About() {
 	return (
-		<div className='m-5'>
+		<div className='m-5 container'>
 			<h3>About Me</h3>
 			<img
 				alt='Profile'
 				src='https://avatars.githubusercontent.com/u/85598391?v=4'
-				className='mx-2'
+				className='mx-2 mt-4'
 			/>
+			<p className='lead my-4 pb-5'>
+				Hello! My name is Douglas Peterson and I am a full stack developer. From
+				Bootcamp, to self taught and on the job experience, I've learned
+				adaptable skills that can be applied to any project.
+			</p>
 		</div>
 	);
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,29 @@
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
 import React from 'react';
 
 function Footer() {
-	return <></>;
+	return (
+		<footer className='Footer d-flex justify-content-center'>
+			<a
+				href='https://www.linkedin.com/in/douglas-peterson/'
+				target='_blank'
+				rel='noopener noreferrer'
+				className='m-3'
+			>
+				<FaLinkedin size={32} />
+			</a>
+
+			<a
+				href='https://github.com/doug-coder64/'
+				target='_blank'
+				rel='noopener noreferrer'
+				className='m-3'
+			>
+				<FaGithub size={32} />
+			</a>
+		</footer>
+	);
 }
 
 export default Footer;
